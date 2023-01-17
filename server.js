27af979cbdaf60218255b5e2135c72e98996d453
roel12037 <84542925+ROEL12037@users.noreply.github.com>
2023-01-16 23:14:49 -0600

    add dev dependencies

--- server.js
+++ server.js
@@ -2,11 +2,14 @@ require('dotenv').config( {path: './config/.env'} )
 
 const express = require('express')
 const app = express()
+const logger = require('morgan')
 
 const {connectDB} = require('./config/db')
 
 connectDB()
 
+app.use(logger('dev'))
+
 app.listen(process.env.PORT, () => {
-    console.log(`server running on port: ${process.env.PORT}`)
+    console.log(`server running in ${process.env.NODE_ENV} mode on port: ${process.env.PORT}`)
 })
