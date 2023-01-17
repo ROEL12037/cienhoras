41afb47e75a2c6af8c8f4e2edcf0c5f1b6adf412
roel12037 <84542925+ROEL12037@users.noreply.github.com>
2023-01-16 23:34:17 -0600

    add logger

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 require('dotenv').config( {path: './config/.env'} )
 
 const express = require('express')
-const app = express()
 const logger = require('morgan')
 
-const {connectDB} = require('./config/db')
+const { connectDB } = require('./config/db')
 
 connectDB()
 
-app.use(logger('dev'))
+const app = express()
+
+if (process.env.NODE_ENV === 'development') {
+    app.use(logger('dev'))
+}
 
 app.listen(process.env.PORT, () => {
     console.log(`server running in ${process.env.NODE_ENV} mode on port: ${process.env.PORT}`)
