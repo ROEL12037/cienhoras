eb22db71773aa1e78e46ee92c47451c3b2da4e45
roel12037 <84542925+ROEL12037@users.noreply.github.com>
2023-01-16 22:44:40 -0600

    add connection to mongoDB

---
+++ config/db.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+
+const connectDB = async () => {
+    try {
+        const conn = await mongoose.connect(process.env.URI)
+        // console.log(conn.connection.host)
+        console.log(`connected to ${conn.connection.name} DB on HOST: ${conn.connection.host}`)
+    } catch (error) {
+        console.error(error)
+        process.exit(1)
+    }
+}
+
+module.exports = {
+    connectDB
+}
--- server.js
+++ server.js
@@ -3,6 +3,10 @@ require('dotenv').config( {path: './config/.env'} )
 const express = require('express')
 const app = express()
 
+const {connectDB} = require('./config/db')
+
+connectDB()
+
 app.listen(process.env.PORT, () => {
     console.log(`server running on port: ${process.env.PORT}`)
 })
