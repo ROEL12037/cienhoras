16010ebe07232e688c9930cb40a762ec81ccc3e0
roel12037 <84542925+ROEL12037@users.noreply.github.com>
2023-01-17 18:58:32 -0600

    authenticate with google and save to database

--- config/passport.js
+++ config/passport.js
@@ -2,8 +2,6 @@ const GoogleStrategy = require('passport-google-oauth20').Strategy
 const mongoose = require('mongoose')
 const User = require('../models/UserModel')
 
-
-
 module.exports = function(passport) {
     passport.use(new GoogleStrategy({
         clientID: process.env.GOOGLE_CLIENT_ID,
@@ -12,6 +10,25 @@ module.exports = function(passport) {
     },
     async (accessToken, refreshToken, profile, done) => {
         console.log(profile)
+        const newUser = {
+            googleId: profile.id,
+            displayName: profile.displayName,
+            firstName: profile.name.givenName, 
+            lastName: profile.name.familyName,
+            image: profile.photos[0].value
+        }
+
+        try {
+            let user = await User.findOne({googleId: profile.id})
+            if (user) {
+                done(null, user)
+            } else {
+                user = await User.create(newUser)
+                done(null, user)
+            }
+        } catch (error) {
+            console.error(error)
+        }
     }))
 
     passport.serializeUser((user, done) => {
