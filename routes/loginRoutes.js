f2595a43d2075cb4ebf3a09473b722e2e7ee2cab
roel12037 <84542925+ROEL12037@users.noreply.github.com>
2023-01-17 18:31:45 -0600

    add user model and google strategy

--- routes/loginRoutes.js
+++ routes/loginRoutes.js
@@ -1,8 +1,24 @@
 const router = require('express').Router()
+const passport = require('passport')
 const { getLoginPage } = require('../controllers/loginControllers')
 
-// desc         profile page
-// route        get /profile
+// desc         login page
+// route        get /login
 router.get('/', getLoginPage)
+
+// desc         authenticate with google
+// route        get /login/google
+router.get('/google', passport.authenticate('google', {scope: ['profile']}))
+
+// desc         authenticate with google
+// route        get /login/google/callback
+router.get(
+    '/google/callback', 
+    passport.authenticate('google', {failureRedirect: '/login'}), 
+    (req, res) => {
+        res.redirect('/profile')
+    }
+)
+
 
 module.exports = router
